19516da412ccb764953971863928fc1faf52605c
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2022-01-04 03:30:48 +0000

    add validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,9 +10,9 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Jelly", last_name: "Fish", hourly_rate: 33)
-@store1.employees.create(first_name: "Pillow", last_name: "Case", hourly_rate: 15)
-@store4.employees.create(first_name: "Pizza", last_name: "Panini", hourly_rate: 22)
-@store5.employees.create(first_name: "Yellow", last_name: "Bus", hourly_rate: 76)
-@store5.employees.create(first_name: "Teenage", last_name: "Angst", hourly_rate: 13)
-@store6.employees.create(first_name: "Oscar", last_name: "Wilde", hourly_rate: 14)
+@store2.employees.create(first_name: "Jelly", last_name: "Fish", hourly_rate: 50)
+@store1.employees.create(first_name: "Pillow", last_name: "Case", hourly_rate: 84)
+@store4.employees.create(first_name: "Pizza", last_name: "Panini", hourly_rate: 103)
+@store5.employees.create(first_name: "Yellow", last_name: "Bus", hourly_rate: 99)
+@store5.employees.create(first_name: "Teenage", last_name: "Angst", hourly_rate: 78)
+@store6.employees.create(first_name: "Oscar", last_name: "Wilde", hourly_rate: 40)
