6ac040dd9132c83d373832acb8232e2cf82cfd14
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2022-01-04 02:19:34 +0000

    implement db filter

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+mens_stores = Store.where(mens_apparel: true)
+mens_stores.each { |mens_store| puts "#{mens_store.name} has an annual revenu of #{mens_store.annual_revenue}" }
+
+womens_stores = Store.where(womens_apparel: true)
+womens_stores.each {
+  |womens_store| 
+  if womens_store.annual_revenue < 1000000
+    puts "#{womens_store.name} has an annual revenu of #{womens_store.annual_revenue}"
+  end
+}
